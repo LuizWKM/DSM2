4fcf591e87789d47f28b45b97cf12d70f3d02ad0
Luiz <luizricardowm@outlook.com>
2024-09-06 18:08:55 -0300

    C feito

--- ED/Exercicios/Exercicio_matriz_pratique/Matriz03/main.c
+++ ED/Exercicios/Exercicio_matriz_pratique/Matriz03/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAML 2
-#define TAMC 2
+#define TAML 7
+#define TAMC 5
 int main()
 {
     int mat[TAML][TAMC];
-    int l = 0, c = 0, maior = 0;
+    int l = 0, c = 0, maior = 0, lm = 0, cm = 0;
 
     for(l = 0; l < TAML;l++){
         for(c = 0; c < TAMC;c++){
@@ -14,8 +14,10 @@ int main()
             scanf("%d",&mat[l][c]);
             if(maior <= mat[l][c]){
                 maior = mat[l][c];
+                lm = l;
+                cm = c;
             }
         }
     }
-    printf("%d",maior);
+    printf("Maior numero: %d\nLinha: %d\nColuna: %d",maior, lm, cm);
 }
